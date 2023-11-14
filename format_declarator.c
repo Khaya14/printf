532c2f51e137485ad9fb37b8c067a05a24bd4e22
Khaya14 <khayashezi14@gmail.com>
2023-11-14 14:53:34 -0800

    Printf

--- format_declarator.c
+++ format_declarator.c
@@ -27,7 +27,23 @@ break;
 case 'd': case 'i':
 val += prt_int((va_arg(list, int)));
 break;
+case 'x':
+val += hexa_int(va_arg(list, unsigned int), 0);
+break;
+case 'X':
+val += hexa_int(va_arg(list, unsigned int), 1);
+break;
+case 'u':
+val += prt_long_int(va_arg(list, unsigned int));
+break;
+case 'o':
+val += octal_int(va_arg(list, unsigned int));
+break;
+case 'b':
+val += binary_num(va_arg(list, unsigned int));
+break;
 default:
+val += _putchar('%');
 val += _putchar(format_dec);
 break;
 }
